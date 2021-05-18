94a67af5d1e27b38dc69c450d980a2c2e2993cd7
SAED ALLOUZI <saedallouzi@Samers-MacBook-Pro.local>
2021-05-18 21:08:17 +0300

    storytask

---
+++ story.js
@@ -0,0 +1,8 @@
+let name  = "saed" ;
+let age = 25 ;
+let hobby = "playing football" ;
+
+
+
+console.log(`Mr. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`)
+
